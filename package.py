ce0835305282705ad5af9547db541fbb2fc26d02
Fredrik Brännbacka FL <fredrik.brannbacka@ilpvfx.com>
2017-08-20 17:46:15 +0200

    Fixed cmake

--- package.py
+++ package.py
@@ -8,12 +8,13 @@ authors = ['fredrik.brannbacka']
 
 variants = [["platform-linux"]]
 
-requires = [
+build_requires = [
     "jpeg",
     "zlib"
 ]
 
 def commands():
+    env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.prepend("{root}/lib")
     if building:
         env.CMAKE_MODULE_PATH.append("{root}/cmake")
